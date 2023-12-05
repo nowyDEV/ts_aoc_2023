82a1c532538908166039e01930c344d10c9566a3
nowyDEV <dominik.nowik@nordcloud.com>
2023-12-05 01:22:52 +0100

    day 1 part 1

--- day_01/day_01.ts
+++ day_01/day_01.ts
@@ -1,1 +1,22 @@
-console.log("haha");
+import input from "./input.txt";
+
+const data = input.split(/\r?\n/);
+
+const regexp = /\d+/g;
+
+let sum = 0;
+
+for (const line of data) {
+  const match = (line.match(regexp) ?? []).join("");
+
+  const num =
+    match === ""
+      ? "0"
+      : match.length === 1
+      ? match + match
+      : match[0] + match.at(-1);
+
+  console.log({ num });
+
+  sum += parseInt(num);
+}
